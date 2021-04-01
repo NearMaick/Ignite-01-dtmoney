c6f5e2d3865d2fc8a8f66c7b8b3290a121f06e58
NearMaick <maick_a_s@msn.com>
2021-03-31 22:20:35 -0300

    chore(web): GlobalStyle

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,11 @@
+import { GlobalStyle } from "./styles/global";
+
 export function App() {
   return (
-    <h1>Maick Souza</h1>
+    <>
+      <h1>Maick Souza</h1>
+      <GlobalStyle />
+    </>
   );
 }
 
