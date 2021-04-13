fae94c9925a386cf9b63d6888513fd9f74756500
Maick Souza <maick_a_s@msn.com>
2021-04-13 14:18:06 -0300

    style(web): TransactionsTable #1

--- src/components/Dashboard/index.tsx
+++ src/components/Dashboard/index.tsx
@@ -1,9 +1,11 @@
 import { Summary } from "../Summary";
+import { TransactionsTable } from "../TransactionsTable";
 
 export function Dashboard() {
   return (
     <>
       <Summary />
+      <TransactionsTable />
     </>
   )
 }
