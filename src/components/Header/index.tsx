77e39dede5bc1cc820e1c0c354a3d0aeb685a4b8
Maick Souza <maick_a_s@msn.com>
2021-04-14 12:01:09 -0300

    feat(web): Modal #2

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -7,17 +7,14 @@ interface HeaderProps {
 }
 
 export function Header({ onOpenNewTransactionModal }: HeaderProps) {
- return(
-   <Container>
+  return(
+    <Container>
       <Content>
         <img src={logoImg} alt="dt money" />
         <button type="button" onClick={onOpenNewTransactionModal}>
           Nova transação
         </button>
-
-        
-
       </Content>
-   </Container>
- ) 
+    </Container>
+  ) 
 }
