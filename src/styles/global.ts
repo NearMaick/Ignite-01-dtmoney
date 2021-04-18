700ff451aa80a48c672100be61dc2ae82405f0f7
Maick Souza <maick_a_s@msn.com>
2021-04-18 11:40:58 -0300

    style(web): Form Modal #2

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -53,7 +53,7 @@ export const GlobalStyle = createGlobalStyle`
     cursor: not-allowed;
   }
 
-  .react-modal-overlay{
+  .react-modal-overlay {
     background: rgba(0, 0, 0, 0.5);
 
     position: fixed;
@@ -67,12 +67,26 @@ export const GlobalStyle = createGlobalStyle`
     justify-content: center;
   }
 
-  .react-modal-content{
+  .react-modal-content {
     width: 100%;
     max-width: 576px;
     background: var(--background);
     padding: 3rem;
     position: relative;
     border-radius: 0.25rem;
+  }
+
+  .react-modal-close {
+    position: absolute;
+    right: 1.5rem;
+    top: 1.5rem;
+    border: 0;
+    background: transparent;
+
+    transition: filter 0.4s;
+
+    &:hover {
+      filter: brightness(0.8);
+    }
   }
 `
